1f9f7b858190a245054f5af0437450ef86ba3a6f
Ray Chen <half0wl@protonmail.ch>
2017-03-12 05:14:33 +0800

    Simplify bytes2human()

--- simon.py
+++ simon.py
@@ -20,6 +20,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import math
 import psutil
 from PyObjCTools import AppHelper
 from Foundation import NSTimer, NSRunLoop
@@ -27,22 +28,13 @@ from AppKit import NSApplication, NSStatusBar, NSMenu, NSMenuItem, \
     NSEventTrackingRunLoopMode
 
 
-# https://github.com/giampaolo/psutil/blob/master/scripts/meminfo.py
 def bytes2human(n):
-    # http://code.activestate.com/recipes/578019
-    # >>> bytes2human(10000)
-    # '9.8K'
-    # >>> bytes2human(100001221)
-    # '95.4M'
-    symbols = (' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB')
-    prefix = {}
-    for i, s in enumerate(symbols):
-        prefix[s] = 1 << (i + 1) * 10
-    for s in reversed(symbols):
-        if n >= prefix[s]:
-            value = float(n) / prefix[s]
-            return '%.1f%s' % (value, s)
-    return "%sB" % n
+    # Credits to /u/cyberspacecowboy on reddit
+    # https://www.reddit.com/r/Python/comments/5xukpd/-/dem5k12/
+    symbols = (' B', ' KiB', ' MiB', ' GiB', ' TiB', ' PiB', ' EiB', ' ZiB',
+               ' YiB')
+    i = math.floor(math.log(abs(n)+1, 2) / 10)
+    return '%.1f%s' % (n/2**(i*10), symbols[i])
 
 
 class Simon(NSApplication):
